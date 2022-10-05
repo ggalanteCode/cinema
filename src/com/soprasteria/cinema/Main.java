6fd0855338cae71e7731179c160b72d4e5c6559d
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-10-05 16:59:53 +0200

    sconto del mercoledì

--- src/com/soprasteria/cinema/Main.java
+++ src/com/soprasteria/cinema/Main.java
@@ -6,13 +6,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int etaUtente, prezzoBiglietto = 0;
+		int etaUtente, prezzoBiglietto = 0, sconto = 2;
+		String giorno;
 		
 		Scanner s = new Scanner(System.in);
 		
 		System.out.print("Quanti anni hai?: ");
 		etaUtente = s.nextInt();
 		s.nextLine();
+		System.out.print("Che giorno è?: ");
+		giorno = s.nextLine();
 		
 		if(etaUtente < 7) {
 			prezzoBiglietto = 0;
@@ -20,6 +23,10 @@ public class Main {
 			prezzoBiglietto = 6;
 		} else if(etaUtente >= 15) {
 			prezzoBiglietto = 10;
+		}
+		
+		if(giorno.equalsIgnoreCase("mercoledi")) {
+			prezzoBiglietto -= sconto;
 		}
 		
 		System.out.println("Il prezzo del biglietto è: " + prezzoBiglietto);
