1980e316923e530afedabaeb3a01a5db9420cda4
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-10-05 16:42:33 +0200

    inserimento età dell'utente

--- src/com/soprasteria/cinema/Main.java
+++ src/com/soprasteria/cinema/Main.java
@@ -1,9 +1,16 @@
 package com.soprasteria.cinema;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int etaUtente;
+		
+		Scanner s = new Scanner(System.in);
+		
+		System.out.print("Quanti anni hai?: ");
 		
 	}
 
