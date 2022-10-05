889831a86f66adaf4f4d1da82543e6e49806be38
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-10-05 16:51:24 +0200

    stampa a video del prezzo del biglietto

--- src/com/soprasteria/cinema/Main.java
+++ src/com/soprasteria/cinema/Main.java
@@ -6,13 +6,23 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int etaUtente;
+		int etaUtente, prezzoBiglietto = 0;
 		
 		Scanner s = new Scanner(System.in);
 		
 		System.out.print("Quanti anni hai?: ");
 		etaUtente = s.nextInt();
 		s.nextLine();
+		
+		if(etaUtente < 7) {
+			prezzoBiglietto = 0;
+		} else if(etaUtente >= 7 && etaUtente < 15) {
+			prezzoBiglietto = 6;
+		} else if(etaUtente >= 15) {
+			prezzoBiglietto = 10;
+		}
+		
+		System.out.println("Il prezzo del biglietto è: " + prezzoBiglietto);
 	}
 
 }
